4d36dc478c99097ae44e07408bb5e45d68a9b7e6
belenbareiro <belenbareiro09@gmail.com>
2024-08-17 20:50:26 -0300

    Inicialice de nuevo el proyecto usando la version de vue que se muestra en el curso

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <el-menu class="menu"
+    >
+      <el-menu-item index="1">
+        <router-link :to="{ path: '/' }" class="custom-menu-item">Home</router-link>
+      </el-menu-item>
+    </el-menu>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+* {
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+
+.menu {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+
+}
+
+
+.menu a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
